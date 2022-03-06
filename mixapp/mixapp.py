1e7c7f5698857d1f698be2c2904b999f07a819bc
MiyuCode <streamlabsdonationtools@gmail.com>
2022-03-06 21:13:58 +0100

    feat: version 1.0

---
+++ mixapp/mixapp.py
@@ -0,0 +1,69 @@
+from tkinter import *
+import os
+import sys
+
+class Ressources(object):
+    """docstring for Ressources"""
+    def __init__(self):
+        super(Ressources, self).__init__()
+    nameapp = ""
+
+if len(sys.argv[1]) > 0:
+    if sys.argv[1] == "-createapp":
+        if len(sys.argv[2]) > 0:
+            # Print output
+            # print(f"Nom: {sys.argv[2]}")
+            Ressources.nameapp = sys.argv[2]
+            output_file = open(f"{Ressources.nameapp}.py", "w")
+            output_file.write(f"print(\"Welcome on {Ressources.nameapp} !\")")
+            output_file.close()
+        else:
+            print("MixApp> Enter a correct name for your app !")
+    elif sys.argv[1] == "-deleteapp":
+        if len(sys.argv[2]) > 0:
+            nameofapp = sys.argv[2]
+            os.remove(nameofapp + ".py")
+            print(f"MixApp> \"{nameofapp}\" has been deleted.")
+        else:
+            print("MixApp> Error has been detected.")
+    elif sys.argv[1] == "-editapp":
+        if len(sys.argv[2]) > 0:
+            if sys.argv[2] == "name":
+                if len(sys.argv[3]) > 0:
+                    if len(sys.argv[4]) > 0:
+                        os.rename(f"{sys.argv[3]}.py", f"{sys.argv[4]}.py")
+                        Ressources.nameapp = sys.argv[4]
+                    else:
+                        print("MixApp> Error has been detected.")
+                else:
+                    print("MixApp> Error has been detected.")
+            elif sys.argv[2] == "gui":
+                if len(sys.argv[3]) > 0:
+                    def notepad():
+                        def savefile():
+                            t = text.get("1.0", "end-1c")
+                            npgui.destroy()
+                            file = open(f'{sys.argv[3]}.py', "w")
+                            file.write(t)
+                            file.close()
+
+                        npgui = Tk()
+                        npgui.title(f'Editing {sys.argv[3]}.py')
+                        npgui.geometry('850x500')
+                        npgui.resizable(False, False)
+                        text = Text(npgui)
+                        text.pack()
+                        button = Button(npgui, text="Enregistrer", command=savefile)
+                        button.pack()
+                        npgui.mainloop()
+                    notepad()
+                else:
+                    print("MixApp> Error has been detected.")
+            else:
+                print("MixApp> Error has been detected.")
+        else:
+            print("MixApp> Error has been detected.")
+    else:
+        print("MixApp> Error has been detected.")
+else:
+    print("MixApp> Error has been detected.")
